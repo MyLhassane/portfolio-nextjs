a0ff6a81e83d06987fc91455569a9674db804e31
My Lhassane <mylhassanedeveloper@gmail.com>
2022-08-17 22:46:32 +0100

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -89,7 +89,7 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
-                <Image src="/assets/skills/shopify.png" width="64px" height="64px" alt="Shopify" />
+                <Image src="/assets/skills/wordpress.png" width="64px" height="64px" alt="Shopify" />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <h3>Shopify</h3>
